1120a570b13d1a7bfe668823659a208f273a34c9
Justyna Luszczak <justynasylwia017@gmail.com>
2020-05-30 16:31:42 +0200

    Adding properties in Lectures.cs

--- Meetup_Project/Meetup_Project/Entities/Lectures.cs
+++ Meetup_Project/Meetup_Project/Entities/Lectures.cs
@@ -7,5 +7,27 @@ namespace Meetup_Project.Entities
 {
     public class Lectures
     {
+
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Topic { get; set; }
+        public string Description { get; set; }
+        public virtual Meetup Meetup { get; set; }
+        public int MeetupId { get; set; }
+
+        public Lectures()
+        {
+            
+        }
+
+        public Lectures(int id, string author, string topic, string description, Meetup meetup, int meetupId)
+        {
+            Id = id;
+            Author = author;
+            Topic = topic;
+            Description = description;
+            Meetup = meetup;
+            MeetupId = meetupId;
+        }
     }
 }
