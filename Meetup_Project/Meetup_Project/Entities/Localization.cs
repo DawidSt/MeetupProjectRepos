5917df940a996124e4a02fe1aed16cd3d225e8f7
Dawid Stanclik <dawidstanclik@wp.pl>
2020-05-30 13:29:45 +0200

    Add Entities

---
+++ Meetup_Project/Meetup_Project/Entities/Localization.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meetup_Project.Entities
+{
+    public class Localization
+    {
+    }
+}
