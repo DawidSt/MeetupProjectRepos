1fef7a642d1985664d3cdad12f21d477fb6af54a
Justyna Luszczak <justynasylwia017@gmail.com>
2020-05-30 16:56:02 +0200

    Adding properties in Localization.cs

--- Meetup_Project/Meetup_Project/Entities/Localization.cs
+++ Meetup_Project/Meetup_Project/Entities/Localization.cs
@@ -7,5 +7,12 @@ namespace Meetup_Project.Entities
 {
     public class Localization
     {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostCode { get; set; }
+        public virtual Meetup Meetup { get; set; }
+        public int MeetupId { get; set; }
+        
     }
 }
