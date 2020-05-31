03bb3f115d5893e74e75a72a968233a6443a3f78
Justyna Luszczak <justynasylwia017@gmail.com>
2020-05-31 23:07:34 +0200

    Adding properties in Meetup.cs

--- Meetup_Project/Meetup_Project/Entities/Meetup.cs
+++ Meetup_Project/Meetup_Project/Entities/Meetup.cs
@@ -7,5 +7,25 @@ namespace Meetup_Project.Entities
 {
     public class Meetup
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Organizer { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsPrivate { get; set; }
+        public virtual ICollection<Lectures> Lectures { get; set; } = new List<Lectures>();
+        public virtual Localization Location { get; set; }
+
+        public Meetup()
+        {
+            
+        }
+
+        public Meetup(string name, string organizer, DateTime date, bool isPrivate)
+        {
+            Name = name;
+            Organizer = organizer;
+            Date = date;
+            IsPrivate = isPrivate;
+        }
     }
 }
