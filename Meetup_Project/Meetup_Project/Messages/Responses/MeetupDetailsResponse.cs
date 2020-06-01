762c22102660a2c8ae41e93245d73976e027df9b
Justyna Luszczak <justynasylwia017@gmail.com>
2020-06-02 00:55:48 +0200

    Added properties, list and constructor

--- Meetup_Project/Meetup_Project/Messages/Responses/MeetupDetailsResponse.cs
+++ Meetup_Project/Meetup_Project/Messages/Responses/MeetupDetailsResponse.cs
@@ -7,5 +7,50 @@ namespace Meetup_Project.Messages.Responses
 {
     public class MeetupDetailsResponse
     {
+        public string Name { get; }
+        public string Organizer { get; }
+        public DateTime Date { get; }
+        public bool IsPrivate { get; }
+        public string City { get; }
+        public string Street { get; }
+        public string PostCode { get; }
+        public List<LecturesDto> Lectures { get; }
+
+        public MeetupDetailsResponse(
+            string name,
+            string organizer,
+            DateTime date,
+            bool isPrivate,
+            string city,
+            string street,
+            string postCode,
+            
+            
+            List<LecturesDto> lectures) {
+            Name = name;
+            Organizer = organizer;
+            Date = date;
+            IsPrivate = isPrivate;
+            City = city;
+            Street = street;
+            PostCode = postCode;
+            Lectures = lectures;
+        }
+
+        public class LecturesDto
+        {
+            public int Id { get; }
+            public string Author { get; }
+            public string Topic { get; }
+            public string Description { get; }
+
+            public LecturesDto(int id, string author, string topic, string description)
+            {
+                Id = id;
+                Author = author;
+                Topic = topic;
+                Description = description;
+            }
+        }
     }
 }
