90a2462b62a20842ddf5f428c587415cb1e02b48
Justyna Luszczak <justynasylwia017@gmail.com>
2020-06-02 01:08:30 +0200

    Added properties

--- Meetup_Project/Meetup_Project/Messages/Responses/MeetupListResponse.cs
+++ Meetup_Project/Meetup_Project/Messages/Responses/MeetupListResponse.cs
@@ -5,7 +5,22 @@ using System.Threading.Tasks;
 
 namespace Meetup_Project.Messages.Responses
 {
-    public class MeetupListResponse
-    {
+    public class MeetupListResponse {
+             public int Id { get; }
+             public string Name { get; }
+             public string Organizer { get; }
+             public DateTime Date { get; }
+             public bool IsPrivate { get; }
+             public string City { get; }
+     
+             public MeetupListResponse(int id, string name, string organizer, DateTime date, bool isPrivate, string city)
+             {
+                 Id = id;
+                 Name = name;
+                 Organizer = organizer;
+                 Date = date;
+                 IsPrivate = isPrivate;
+                 City = city;
+             }
     }
 }
