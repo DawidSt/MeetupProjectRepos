f404b0789e5bfd7ee0ac64266fdb89b5630ac172
Dawid Stanclik <dawidstanclik@wp.pl>
2020-06-19 19:38:08 +0200

    add comment

--- meetup/src/modules/addMeetup.js
+++ meetup/src/modules/addMeetup.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 
-
 const AddMeetup = () => {
-    const { handleSubmit, register, control } = useForm({
-        defaultValues: {
-            lectures: [
-                {
-                    author: "",
-                    topic: "",
-                    description: ""
-                }
-            ]
-        }
-    });}
+  const { handleSubmit, register, control } = useForm({
+    defaultValues: {
+      lectures: [
+        {
+          author: "",
+          topic: "",
+          description: "", //ddqwqdwqwd
+        },
+      ],
+    },
+  });
+};
