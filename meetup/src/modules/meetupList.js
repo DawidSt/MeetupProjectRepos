17205933467f25e06156a2be26e65c4843163322
Dawid Stanclik <dawidstanclik@wp.pl>
2020-06-19 20:10:30 +0200

    added function MeetupList

--- meetup/src/modules/meetupList.js
+++ meetup/src/modules/meetupList.js
@@ -4,3 +4,38 @@ import { baseUrl } from "../constants/constants";
 import MeetupListItem from "./meetupListItem";
 import { A } from "hookrouter";
 import "../styles/meetupList.scss";
+
+const MeetupList = () => {
+  const [meetList, setMeetist] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const users = await axios.get(`${baseUrl}/meetup`);
+      setMeetist(users.data);
+    })();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="add">
+        <A className="add__btn" href="/add">
+          Dodaj nowe wydarzenie
+        </A>
+      </div>
+      <div className="container__meetupList">
+        {meetList.map((x, index) => (
+          <MeetupListItem
+            key={index}
+            id={x.id}
+            name={x.name}
+            city={x.city}
+            date={x.date}
+            organizer={x.organizer}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MeetupList;
