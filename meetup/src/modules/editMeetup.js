15d2f45d5ee109d7e48b9a3c65ecd0e27a317355
Dawid Stanclik <dawidstanclik@wp.pl>
2020-06-21 12:02:10 +0200

    edit editMeetup.js - added imports

--- meetup/src/modules/editMeetup.js
+++ meetup/src/modules/editMeetup.js
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {baseUrl} from "../constants/constants";
+import { baseUrl } from "../constants/constants";
+import { useFieldArray, useForm } from "react-hook-form";
+import { navigate } from "hookrouter";
 
+const EditMeetup = ({ id }) => {
+  const [meetup, setMeetup] = useState({});
 
-const EditMeetup = ({id}) => {
-    const [meetup, setMeetup] = useState({});
-
-    useEffect(() => {
-        (async () => {
-            const users = await axios.get(
-                `${baseUrl}/meetup/${id}`);
-            setMeetup(users.data);
-        })();
-    }, [id]);}
-
+  useEffect(() => {
+    (async () => {
+      const users = await axios.get(`${baseUrl}/meetup/${id}`);
+      setMeetup(users.data);
+    })();
+  }, [id]);
+};
