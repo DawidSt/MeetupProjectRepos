b6c94011d1120fa744bbcf208a288825a30a45f0
Dawid Stanclik <dawidstanclik@wp.pl>
2020-09-07 20:29:40 +0200

    Added meetupDetails.scss, update routes.js

--- meetup/src/routes.js
+++ meetup/src/routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import MeetupList from "./modules/meetupList";
-//import MeetupDetails from "./modules/meetupDetails";
+import MeetupDetails from "./modules/meetupDetails";
 import EditMeetup from "./modules/editMeetup";
 import AddMeetup from "./modules/addMeetup";
 
 const routes = {
   "/": () => <MeetupList />,
   "/add": () => <AddMeetup />,
-  //"/details/:id": ({ id }) => <MeetupDetails id={id} />,
+  "/details/:id": ({ id }) => <MeetupDetails id={id} />,
   "/edit/:id": ({ id }) => <EditMeetup id={id} />,
 };
 
